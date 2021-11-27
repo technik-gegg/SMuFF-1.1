1abbe18d737ed074032e36a6066ece2c279ff586
Technik Gegg <44457073+technik-gegg@users.noreply.github.com>
2021-11-27 15:04:38 +0100

    Added header pins in comments

--- include/SKR_mini_E3DIP/DSP_Minipanel.h
+++ include/SKR_mini_E3DIP/DSP_Minipanel.h
@@ -23,16 +23,16 @@
 #pragma message "Compiling for FYSETC Mini12864 Panel V2.0 and 2.1"
 
 #define DSP_DATA_PIN        -1      // USE MOSI ON SPI1 HEADER
-#define DSP_CS_PIN          PB9     // DOGLCD_CS
-#define DSP_DC_PIN          PB6     // DOGLCD_A0
+#define DSP_CS_PIN          PB9     // EXP1.9 (DOGLCD_CS)
+#define DSP_DC_PIN          PB6     // EXP1.5 (DOGLCD_A0)
 #define DSP_RESET_PIN       PA13    // SWDIO --- IMPORTANT: This display needs a RESET signal!
 
-#define ENCODER1_PIN        PA9
-#define ENCODER2_PIN        PA10
-#define ENCODER_BUTTON_PIN  PB8
+#define ENCODER1_PIN        PA9     // EXP1.8
+#define ENCODER2_PIN        PA10    // EXP1.6
+#define ENCODER_BUTTON_PIN  PB8     // EXP1.4
 
 #define BEEPER_PIN          PA15    // EXP1.10
 
 #if defined(USE_MINI12864_PANEL_V21)
-#define NEOPIXEL_PIN        PB7     // for display backlight
+#define NEOPIXEL_PIN        PB7     // for display backlight (EXP1.3)
 #endif
