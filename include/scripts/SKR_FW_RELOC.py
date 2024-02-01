d13c3ac2b134fcd4b60c0e35a1366178e5cb238a
Technik Gegg <44457073+technik-gegg@users.noreply.github.com>
2024-02-01 06:27:01 +0100

    fixed: deque mutated during iteration

--- include/scripts/SKR_FW_RELOC.py
+++ include/scripts/SKR_FW_RELOC.py
@@ -7,14 +7,16 @@ Import("env")
 flash_ofs = "0x7000"    # default location is on top of bootloader 
 
 # Lookup for user defined "flash offset" and use this instead of the predefined value
-for define in env['CPPDEFINES']:
+envdefs = env['CPPDEFINES'].copy()
+for define in envdefs:
     if define[0] == "FLASH_OFFSET":
         flash_ofs = define[1]
     if define[0] == "VECT_TAB_OFFSET":
         env['CPPDEFINES'].remove(define)
 env['CPPDEFINES'].append(("VECT_TAB_OFFSET", flash_ofs))
-        
-for lflag in env['LINKFLAGS']:
+
+envlnkf = env['LINKFLAGS'].copy()
+for lflag in envlnkf:
     if lflag.startswith("-Wl,--defsym=LD_FLASH_OFFSET"):
         env['LINKFLAGS'].remove(lflag)
 
